b2f97e07ae09fd6d67314dfb8c6d10762e088e60
Erik Uecke <erik.uecke@digitalfilmacademy.edu>
2016-12-28 13:14:36 -0500

    UI Layout

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Erik Uecke on 12/28/16.
+//  Copyright © 2016 Erik Uecke. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+
+
+}
+
